565d0b2dae0fce70acf94fb4f3f3b9c0a7c3dfa8
hanjun88 <xx130580550@yahoo.com>
2026-09-16 08:00:32 +0700

    fix(test): exclude runner script from Jest discovery

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
   testMatch: ['**/*.test.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/tests/runner.test.ts'],
   transform: {
     '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.test.json' }]
   },
